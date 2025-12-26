1e285764a40ada58fdbd2b01b8d44c26f62a1519
khiem <khiemnguyen.hye@gmail.com>
2025-12-27 03:36:25 +0700

    Adjust navbar item horizontal distribution.

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -5,12 +5,12 @@ import { HoverCard, HoverCardContent, HoverCardTrigger } from "@radix-ui/react-h
 
 const NavBar = () => {
   return (
-    <nav className="sticky top-0 flex items-center justify-between w-full p-3 border-b-2 pl-10">
-      <div className="flex items-center gap-3">
+    <nav className="sticky top-0 flex items-center w-full p-3 border-b-2 pl-10">
+      <div className="flex flex-1 items-center gap-3">
         <img src={logo} className="h-8 hover:drop-shadow-[0_0_5px_rgba(60,130,240,1)] transition-all" />
         <p className="text-xl hover:drop-shadow-[0_0_10px_rgba(60,130,240,1)] transition-all">Katana ID</p>
       </div>
-      <div className="flex items-center gap-3">
+      <div className="flex flex-1 items-center justify-center gap-3">
         <HoverCard openDelay={200} closeDelay={400}>
           <HoverCardTrigger asChild>
             <Button variant="ghost">Developers</Button>
@@ -23,7 +23,7 @@ const NavBar = () => {
         <Button variant="ghost">Demo</Button>
         <Button variant="ghost">Contact</Button>
       </div>
-      <div className="flex items-center gap-3">
+      <div className="flex flex-1 items-center justify-end gap-3">
         <Button variant="ghost" className="">Sign in</Button>
         <Button variant="default" className="">Verify a media <ArrowRight className="ml-5" /></Button>
       </div>
