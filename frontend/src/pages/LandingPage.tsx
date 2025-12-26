9bb07a4980e08b7a8839b02da9496a4d7de1b581
khiem <khiemnguyen.hye@gmail.com>
2025-12-27 03:28:07 +0700

    Add landing page hero with deepfake check button.

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,6 +1,19 @@
+import { Button } from "@/components/ui/button"
+import { LucideArrowRight, LucideSearch, LucideSearchCheck, LucideSearchCode } from "lucide-react"
+
 const LandingPage = () => {
   return (
-    <h1>Detect Deepfake Content with Precision</h1>
+    <>
+      <div className="flex flex-col justify-center pt-20 items-center font-bold text-7xl">
+        <Button variant={"outline"} className="group overflow-hidden rounded-2xl">
+          <LucideSearch className="group-hover:translate-x-40 transition-all" />
+          Check for deepfake
+          <LucideSearchCheck className="hidden group-hover:block mr-2 transition-all"/>
+        </Button>
+        <h1 className="mt-10">Detect Deepfake Content</h1>
+        <h1 className="mt-5">with Precision</h1>
+      </div>
+    </>
   )
 }
 
