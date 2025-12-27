33cde824e99bf137469e8e3b02e0e963b4ac4c92
khiem <khiemnguyen.hye@gmail.com>
2025-12-28 01:41:45 +0700

    Complete auth store

--- frontend/store/useAuthStore.ts
+++ frontend/store/useAuthStore.ts
@@ -1,70 +1,78 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { axiosInstance } from "../src/lib/axios";
 import { AxiosError } from "axios";
 
-export const useAuthStore = create<AuthStore>()((set) => ({
-  authUser: null,
-  token: null,
-  isSigningUp: false,
-  isLoggingIn: false,
+export const useAuthStore = create<AuthStore>()(
+  persist(
+    (set) => ({
+      authUser: null,
+      token: null,
+      isSigningUp: false,
+      isLoggingIn: false,
 
-  signup: async (signupData: {
-    username: string;
-    email: string;
-    password: string;
-  }) => {
-    set({ isSigningUp: true });
-    try {
-      const res = await axiosInstance.post("/signup", signupData, {});
-      set({
-        token: res.data.token,
-        authUser: {
-          username: res.data.username,
-          email: res.data.email,
-        },
-      });
-      // TODO: Success message
-    } catch (error: unknown) {
-      if (error instanceof AxiosError) {
-        console.log("Axios error:", error.response?.data);
-        // TODO: Error message
-      } else {
-        console.log("Unknown error:", error);
-        // TODO: Error message
-      }
-    } finally {
-      set({ isSigningUp: false });
+      signup: async (signupData: {
+        username: string;
+        email: string;
+        password: string;
+      }) => {
+        set({ isSigningUp: true });
+        try {
+          const res = await axiosInstance.post("/signup", signupData, {});
+          set({
+            token: res.data.token,
+            authUser: {
+              username: res.data.username,
+              email: res.data.email,
+            },
+          });
+          // TODO: Success message
+        } catch (error: unknown) {
+          if (error instanceof AxiosError) {
+            console.log("Axios error:", error.response?.data);
+            // TODO: Error message
+          } else {
+            console.log("Unknown error:", error);
+            // TODO: Error message
+          }
+        } finally {
+          set({ isSigningUp: false });
+        }
+      },
+
+      login: async (loginData: { email: string; password: string }) => {
+        set({ isLoggingIn: true });
+        try {
+          const res = await axiosInstance.post("/login", loginData, {});
+          set({
+            token: res.data.token,
+            authUser: {
+              username: res.data.username,
+              email: res.data.email,
+            },
+          });
+          // TODO: success message
+        } catch (error: unknown) {
+          if (error instanceof AxiosError) {
+            console.log("Axios error:", error);
+            // TODO: error message
+          } else {
+            console.log("Unknown error:", error);
+          }
+        } finally {
+          set({ isLoggingIn: false });
+        }
+      },
+
+      logout: async () => {
+        set({ token: null, authUser: null });
+      },
+    }),
+    {
+      name: "auth-storage",
     }
-  },
-
-  login: async (loginData: { email: string; password: string }) => {
-    set({ isLoggingIn: true });
-    try {
-      const res = await axiosInstance.post("/login", loginData, {});
-      set({
-        token: res.data.token,
-        authUser: {
-          username: res.data.username,
-          email: res.data.email,
-        },
-      });
-      // TODO: success message
-    } catch (error: unknown) {
-      if (error instanceof AxiosError) {
-        console.log("Axios error:", error);
-        // TODO: error message
-      } else {
-        console.log("Unknown error:", error);
-      }
-    } finally {
-      set({ isLoggingIn: false });
-    }
-  },
-
-  logout: async () => {
-    // TODO: loggout
-  },
-}));
+  )
+);
 
 interface AuthStore {
   authUser: {
