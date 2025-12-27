4064d8a87c7ec96d0a6b6789b939160f4c80cd53
khiem <khiemnguyen.hye@gmail.com>
2025-12-27 12:13:16 +0700

    Add registeration page

---
+++ frontend/src/pages/SignupPage.tsx
@@ -0,0 +1,11 @@
+import { SignupForm } from "@/components/signup-form"
+
+const SignupPage = () => {
+  return (
+    <div className="flex justify-center items-center pt-20">
+      <SignupForm />
+    </div>
+  )
+}
+
+export default SignupPage
