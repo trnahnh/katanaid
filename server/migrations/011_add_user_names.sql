c459202ecdcbe2c5166e1324dd3c3eb535d27581
Anh D Tran <tran3ah@mail.uc.edu>
2026-01-15 17:47:39 +0700

    Fixing goose annotations database migration

--- server/migrations/011_add_user_names.sql
+++ server/migrations/011_add_user_names.sql
@@ -1,6 +1,7 @@
--- Add optional name fields for user profile
+-- +goose Up
 ALTER TABLE users ADD COLUMN first_name VARCHAR(100);
 ALTER TABLE users ADD COLUMN last_name VARCHAR(100);
 
--- Index for potential future queries by name
-CREATE INDEX idx_users_names ON users(first_name, last_name) WHERE first_name IS NOT NULL;
+-- +goose Down
+ALTER TABLE users DROP COLUMN first_name;
+ALTER TABLE users DROP COLUMN last_name;
